500b1ac10d91a7a099f46eb59d2d798af6ca466a
cravingoxygen <vze.ezv@gmail.com>
2022-12-21 13:25:13 +0000

    Partial commit: Added code to save out tasks and the images involved in a task. Also changed checkpointing so that you can specify an experiment name instead of being left to the mercy of the datetime naming

--- utils/logging.py
+++ utils/logging.py
@@ -14,7 +14,7 @@ def print_and_log(log_file, message):
     print(message)
     log_file.write(message + '\n')
 
-def get_log_files(checkpoint_dir, model_path):
+def get_log_files(checkpoint_dir, model_path, experiment_name=None):
     """
     Function that takes a path to a checkpoint directory and returns a reference to a logfile and paths to the
     fully trained model and the model with the best validation score.
@@ -22,9 +22,17 @@ def get_log_files(checkpoint_dir, model_path):
     if model_path:
         model_dir = os.path.split(model_path)[0]
         verify_checkpoint_dir(model_dir)
-    
-    checkpoint_dir = os.path.join(checkpoint_dir, datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
-    os.makedirs(checkpoint_dir)
+    # If no experiment name specified, use timestamp
+    if experiment_name is None:
+        checkpoint_dir = os.path.join(checkpoint_dir, datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+    else:
+    # Otherwise, use the experiment name instead
+        checkpoint_dir = os.path.join(checkpoint_dir, experiment_name)
+
+    if os.path.exists(checkpoint_dir):
+        print("Warning, checkpoint dir {} already exists".format(checkpoint_dir))
+    else:
+        os.makedirs(checkpoint_dir)
 
     checkpoint_path_best = os.path.join(checkpoint_dir, 'best.pt')
     checkpoint_path_final = os.path.join(checkpoint_dir, 'final.pt')
@@ -46,9 +54,9 @@ def stats_to_str(stats):
     s=''
     for stat, scores in stats.items():
         if isinstance(scores, list):
-            s+='{0:}: {1:.2f} ({2:.2f}) '.format(stat, scores[0]*100, scores[1]*100)
+            s+='{0:}: {1:.6f} ({2:.6f}) '.format(stat, scores[0]*100, scores[1]*100)
         else:
-            s+='{0:}: {1:.2f} '.format(stat, scores*100)
+            s+='{0:}: {1:.6f} '.format(stat, scores*100)
     return s
 
 def convert_to_numpy(x):
@@ -70,19 +78,40 @@ def plot_hist(x, bins, filename, checkpoint_dir, user=None, task_num=None, title
     plt.grid(True)
     fname = ""
     if user != None:
-        fname += "{}_".format(user) 
+        fname += "{}_".format(user)
     if task_num != None:
         fname += "{}_".format(task_num)
     fname += filename + ".png"
     plt.savefig(os.path.join(checkpoint_dir, fname))
     plt.close()
 
-
-def save_image_paths(context_clip_paths, target_paths_by_video, seed, checkpoint_dir, task_num):
+def save_task(task_dict, keep_mask, task_num, seed, checkpoint_dir):
     output_dir = os.path.join(checkpoint_dir, "tasks_for_seed_{}".format(seed))
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     file_path = os.path.join(output_dir, "task_{}.pickle".format(task_num))
+
+    with open(file_path, "wb") as file:
+        # Basically the whole task dict, but including the task_num and without the clips.
+        trimmed_task_dict = {  "object_list": task_dict["object_list"],
+                               "context_paths": task_dict["context_paths"],
+                               "target_paths": task_dict["target_paths"],
+                               "context_labels": task_dict["context_labels"],
+                               "target_labels": task_dict["target_labels"],
+                               "context_annotations": task_dict["context_annotations"],
+                               "target_annotations": task_dict["target_annotations"],
+                               "user_id": task_dict["user_id"],
+                               "task": task_num,
+                               "keep_mask": keep_mask,
+                          }
+        pickle.dump(trimmed_task_dict, file)
+
+
+def save_image_paths(context_clip_paths, target_paths_by_video, seed, checkpoint_dir, task_num):
+    output_dir = os.path.join(checkpoint_dir, "paths_for_seed_{}".format(seed))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    file_path = os.path.join(output_dir, "paths_task_{}.pickle".format(task_num))
 
     with open(file_path, "wb") as file:
         clip_path_dict = { "context_clip_paths": context_clip_paths,
@@ -100,4 +129,3 @@ def dump_context_paths_to_file(context_clip_paths, seed, checkpoint_dir):
         for path_arr in context_clip_paths:
             path_list = path_arr.tolist()
             file.writelines(s + "\n" for s in path_list)
-
